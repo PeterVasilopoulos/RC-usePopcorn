0b5aa5c3707ca40a86f83ee833699c6087ac6eb4
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-02-14 17:08:14 -0500

    added prop types

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const containerStyle = {
     display: 'flex',
@@ -8,6 +9,17 @@ const containerStyle = {
 
 const starContainerStyle = {
     display: 'flex',
+}
+
+// Setting the types of all props
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    className: PropTypes.string,
+    messages: PropTypes.array,
+    defaultRating: PropTypes.number,
+    onSetRating: PropTypes.func
 }
 
 // -----------------------------------
